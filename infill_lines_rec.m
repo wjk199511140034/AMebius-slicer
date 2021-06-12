83c607717818026a0326a1e2a2e4c1942d135fad
AMebius <wjk19951114@gmail.com>
2021-06-12 14:49:33 +0800

    Update infill_lines_rec.m

--- infill_lines_rec.m
+++ infill_lines_rec.m
@@ -1,29 +1,24 @@
-function [infill_line] = infill_lines_rec(polygonin,nozzle_dim,infillshell,density,angle,min_l,max_l)
-%polyin=polyshape(polygon);
+function [infill_line] = infill_lines_rec(polygonin,isoffset,density,angle,min_l,max_l)
+global nozzle_dim
 %plot(polygonin)
-%hold on;
-%plot(0,0,'o')
-
 if angle~=0
      polygonin=rotate(polygonin,angle);
-     %plot(polygonin)
-     %plot(min_l,0,'ob')
-     %plot(max_l,0,'o')
      r_limit_min=[cosd(angle) -sind(angle);sind(angle) cosd(angle)]*[min_l;0];
      r_limit_max=[cosd(angle) -sind(angle);sind(angle) cosd(angle)]*[max_l;0];
-     %plot(0,r_limit_max(2),'o')
-     %plot(0,r_limit_min(2),'ob')
      min_l=r_limit_min(2);
      max_l=r_limit_max(2);
 end
-polyout=polybuffer(polygonin,-infillshell);
-
-[x,y]=boundary(polyout);
-polygon=horzcat(x,y);
-if isempty(polygon)
+if isoffset~=0
+    polyout=polybuffer(polygonin,isoffset);
+else
+    polyout=polygonin;
+end
+if isempty(polyout.Vertices)
     infill_line=[];
     return;
 end
+[x,y]=boundary(polyout);
+polygon=horzcat(x,y);
 if density<0
     step=nozzle_dim;
 else
@@ -32,18 +27,18 @@ end
 %step=nozzle_dim;
 %y_list=(min(polygon(:,2)):step:max(polygon(:,2)))';
 y_list=(min_l:step:max_l)';
-%¸ß¶È±í
+%é«˜åº¦è¡¨
 % if max(polygon(:,2))-y_list(end)>=0.2
 %     y_list(length(y_list)+1,:)=max(polygon(:,2));
 % end
 poly_line=[];
 for p=1:size(polygon,1)-1
-    %ÒÀ´ÎÁ¬½Ó¶à±ßĞÎÁ½µãĞÎ³ÉÏß¶Î
+    %ä¾æ¬¡è¿æ¥å¤šè¾¹å½¢ä¸¤ç‚¹å½¢æˆçº¿æ®µ
     k=(polygon(p,2)-polygon(p+1,2))/(polygon(p,1)-polygon(p+1,1));
     if isnan(polygon(p,1)) || isnan(polygon(p+1,1)) || k==0
         continue;
     end
-    %É¾³ıÎŞĞ§ÏßºÍĞ±ÂÊÎª0µÄÏß
+    %åˆ é™¤æ— æ•ˆçº¿å’Œæ–œç‡ä¸º0çš„çº¿
     poly_line=[poly_line;polygon(p,:),polygon(p+1,:),k];
 end
 c_poly_line=NaN(size(y_list,1),100);
@@ -51,137 +46,62 @@ c_poly_line_size=zeros(size(y_list,1),1);
 for i=1:size(poly_line,1)
     node_low = min(poly_line(i,[2 4]));
     node_high = max(poly_line(i,[2 4]));
-    %µ±Ç°±ßµÄ×îµÍµãºÍ×î¸ßµã
+    %å½“å‰è¾¹çš„æœ€ä½ç‚¹å’Œæœ€é«˜ç‚¹
     y_high_index2=find(y_list<=node_high,1,'last');
-    y_low_index2=find(y_list>=node_low,1);
-    %Óëµ±Ç°±ßÏà½»µÄÉ¨ÃæÏßÆğÊ¼ĞĞºÍÖÕÖ¹ĞĞ
+    y_low_index2=find(y_list>node_low,1);
+    %ä¸å½“å‰è¾¹ç›¸äº¤çš„æ‰«é¢çº¿èµ·å§‹è¡Œå’Œç»ˆæ­¢è¡Œ
     
     if y_high_index2 >= y_low_index2 
         for j = y_low_index2:y_high_index2
             c_poly_line_size(j,:) = c_poly_line_size(j,:) + 1;
-            %c_poly_line_sizeÊÇÁĞÏòÁ¿£¬³¤¶ÈÎª×Ü²ãÊı
-            %Óëµ±¶à±ßĞÎ±ßÏßÏà½»µÄ¸ß¶È±àºÅ+1£¬Ñ­»·Íê³ÉËùÓĞÃæÆ¬Ö®ºó£¬c_poly_line_sizeµÄÃ¿Ò»ĞĞ±íÊ¾¶ÔÓ¦yÏà½»µÄ¶à±ßĞÎ±ßÏßÊı
+            %c_poly_line_sizeæ˜¯åˆ—å‘é‡ï¼Œé•¿åº¦ä¸ºæ€»å±‚æ•°
+            %ä¸å½“å¤šè¾¹å½¢è¾¹çº¿ç›¸äº¤çš„é«˜åº¦ç¼–å·+1ï¼Œå¾ªç¯å®Œæˆæ‰€æœ‰é¢ç‰‡ä¹‹åï¼Œc_poly_line_sizeçš„æ¯ä¸€è¡Œè¡¨ç¤ºå¯¹åº”yç›¸äº¤çš„å¤šè¾¹å½¢è¾¹çº¿æ•°
             c_poly_line(j,c_poly_line_size(j)) = i;
-            %c_poly_lineÃ¿Ò»ĞĞ´ú±íÃ¿Ò»¸öyÖµ
-            %Ã¿ĞĞ±íÊ¾Óë¶ÔÓ¦yÏà½»µÄ¶à±ßĞÎ±ßÏß±àºÅ(plineĞĞºÅ)£¬
-            %c_poly_lineÃ¿Ò»ĞĞµÄÓĞĞ§Êı¾İ¸öÊıµÈÓÚc_poly_line_sizeÃ¿ĞĞµÄÖµ
+            %c_poly_lineæ¯ä¸€è¡Œä»£è¡¨æ¯ä¸€ä¸ªyå€¼
+            %æ¯è¡Œè¡¨ç¤ºä¸å¯¹åº”yç›¸äº¤çš„å¤šè¾¹å½¢è¾¹çº¿ç¼–å·(plineè¡Œå·)ï¼Œ
+            %c_poly_lineæ¯ä¸€è¡Œçš„æœ‰æ•ˆæ•°æ®ä¸ªæ•°ç­‰äºc_poly_line_sizeæ¯è¡Œçš„å€¼
         end
     end
 end
 c_lineout=[];
 for k=1:size(y_list,1),h=y_list(k);
-    %Ñ­»·Ã¿Ò»¸ö¸ß¶ÈÖµ
+    %å¾ªç¯æ¯ä¸€ä¸ªé«˜åº¦å€¼
     c_c_p_line_checklist = c_poly_line(k,1:c_poly_line_size(k));
     c_c_p_line=poly_line(c_c_p_line_checklist,:);
-    %c_p_c_lineËùÓĞÓëµ±Ç°¸ß¶ÈÏà½»µÄÏß¶Î
+    %c_p_c_lineæ‰€æœ‰ä¸å½“å‰é«˜åº¦ç›¸äº¤çš„çº¿æ®µ
     c_point=[];
     for c2=1:size(c_c_p_line,1)
-        %ÕÒµ½Ã¿ÌõÏà½»±ßÏß¶ÎÓëµ±Ç°¸ß¶ÈÖµµÄ½»µã
+        %æ‰¾åˆ°æ¯æ¡ç›¸äº¤è¾¹çº¿æ®µä¸å½“å‰é«˜åº¦å€¼çš„äº¤ç‚¹
         if c_c_p_line(c2,5)==-Inf || c_c_p_line(c2,5)==-Inf
             c_point=[c_point;c_c_p_line(c2,1),h];
         else
             c_point=[c_point;c_c_p_line(c2,1)+(h-c_c_p_line(c2,2))/c_c_p_line(c2,5),h];
         end
         %c_point=[c_point;(h-c_c_p_line(c2,2))*(c_c_p_line(c2,3)-c_c_p_line(c2,1))/(c_c_p_line(c2,4)-c_c_p_line(c2,2))+c_c_p_line(c2,1),h];
-        %Á½µãÊ½
+        %ä¸¤ç‚¹å¼
     end
     [c_point_t,~,ib] = unique(c_point,'rows');
     c_point = c_point_t(accumarray(ib,1)==1,:);
-    %ÕûÀícpoint£¬É¾³ıËùÓĞÖØ¸´µã
+    %æ•´ç†cpointï¼Œåˆ é™¤æ‰€æœ‰é‡å¤ç‚¹
     c_line=[];
     if ~isempty(c_point)      
-%         [c_point_t,ind]=unique(c_point,'stable','rows');
-%         ind=setdiff(1:size(c_point,1),ind);
-%         c_point=setdiff(c_point_t,c_point(ind,:),'rows');
         c_point=sortrows(c_point,1)';
-        %ÕûÀícpoint£¬°´xµİÔö,Ã¿ĞĞÒ»¸ö×ø±ê
+        %æ•´ç†cpointï¼ŒæŒ‰xé€’å¢,æ¯è¡Œä¸€ä¸ªåæ ‡
         c_line=reshape(c_point,4,[])';
-        %ÖØËÜ¾ØÕó£¬Ã¿ĞĞÊÇÒ»¸öÏß¶Î£¬½»ÏßÊıÄ¿µÈÓÚĞĞÊı£¬Ïàµ±ÓÚreshape(cpoint2',[],4£¬'by rows')
+        %é‡å¡‘çŸ©é˜µï¼Œæ¯è¡Œæ˜¯ä¸€ä¸ªçº¿æ®µï¼Œäº¤çº¿æ•°ç›®ç­‰äºè¡Œæ•°ï¼Œç›¸å½“äºreshape(cpoint2',[],4ï¼Œ'by rows')
     end
      c_lineout=[c_lineout;c_line];
 end
-if angle~=0
-%     c_start=c_lineout(:,[1 2])';
-%     c_end=c_lineout(:,[3 4])';
-%     c_start=[cosd(-angle) -sind(-angle);sind(-angle) cosd(-angle)]*c_start;
-%     c_end=[cosd(-angle) -sind(-angle);sind(-angle) cosd(-angle)]*c_end;
-%     c_lineout=[c_start',c_end'];
-%     c_lineout(:,[5,6])=0;
-%     count=0;
-%     oldp=[];
-%     while sum(c_lineout(:,5)==0)
-%         if isempty(oldp)
-%             oldp_ind=find(c_lineout(:,5)~=1,1);  
-%             c_lineout(oldp_ind,5)=1;   
-%             oldp=c_lineout(oldp_ind,:);    
-%             count=count+1;      
-%             c_lineout(oldp_ind,6)=count;
-%         end
-%         for ii=1:size(c_lineout,1)
-%             newp=c_lineout(ii,:);
-%             if newp(1)-oldp(1)<(step+0.01) && newp(1)-oldp(1)>(step-0.01) && newp(5)==0
-% %                 if abs(newp(1)-oldp(1))<0.4
-% %                 count=count+1;
-% %                 c_lineout(ii,5)=1;
-% %                 c_lineout(ii,6)=count;
-% %                 oldp=newp;  
-% %                 continue;
-% %                 end
-%                 for i=oldp(2):-0.25:oldp(4)
-%                     % 'down'
-%                     if abs(newp(2)-i)<0.4
-%                     count=count+1;
-%                     c_lineout(ii,5)=1;
-%                     c_lineout(ii,6)=count;
-%                     oldp=newp;  
-%                     continue;
-%                     end       
-%                 end
-%                 for i=newp(4):0.25:newp(2)
-%                     % 'left'
-%                     if abs(i-oldp(1))<0.4 
-%                     count=count+1;
-%                     c_lineout(ii,5)=1;
-%                     c_lineout(ii,6)=count;
-%                     oldp=newp;  
-%                     continue;
-%                     end
-%                 end
-%             end
-%         end 
-%         oldp=[];
-%     end
-%     cpf=sortrows(c_lineout,6);
-%     hold on;
-%     infill_line=[];
-%     for i=1:1:size(cpf,1)
-%         if mod(i,2) == 1
-%         infill_line=[infill_line;cpf(i,[1 2]);cpf(i,[3 4]);NaN NaN];
-%         else
-%         infill_line=[infill_line;cpf(i,[3 4]);cpf(i,[1 2]);NaN NaN];
-%         end
-%     end
-%     return
+if isempty(c_lineout)
+    infill_line=[];
+    %å¦‚æœc_lineoutä¸ºç©ºç»§ç»­æ‰§è¡Œæ’åºï¼Œä¼šç”Ÿæˆé”™è¯¯çš„ç»“æœ
+    return
+else
+    c_lineout(:,[5,6])=0;
 end
-
-
-
-c_lineout(:,[5,6])=0;
 count=0;
 oldp=[];
 while sum(c_lineout(:,5)==0)
-%     if isempty(oldp)
-%         for i=1:size(c_lineout,1)
-%             if c_lineout(i,5)~=1    
-%                 c_lineout(i,5)=1;
-%                 %c_lineout(i,7)=1;
-%                 oldp=c_lineout(i,:);    
-%                 count=count+1;      
-%                 c_lineout(i,6)=count;   
-%                 break;    
-%             end
-%         end
-%     end
     if isempty(oldp)
         oldp_ind=find(c_lineout(:,5)~=1,1);  
         c_lineout(oldp_ind,5)=1;   
@@ -233,7 +153,6 @@ if angle~=0
     c_lineout=[c_lineout2,c_lineout(:,[5 6])];
 end
 cpf=sortrows(c_lineout,6);
-hold on;
 infill_line=[];
 for i=1:1:size(cpf,1)
     if mod(i,2) == 1
@@ -242,6 +161,5 @@ for i=1:1:size(cpf,1)
         infill_line=[infill_line;cpf(i,[3 4]);cpf(i,[1 2]);NaN NaN];
     end
 end
-
 
 end
