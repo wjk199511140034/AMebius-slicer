097b83754a92443ab2164679eaa295a4c6434a2d
AMebius <wjk19951114@gmail.com>
2021-06-12 14:43:18 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,81 +1,89 @@
 %% starting initialization
 clc; clear all; close all;format short;warning('off');
 global bed_hight bed_width bed_temp nozzle_dim filament_dim filament_temp bed_center00
-global layer_hight shell_thick top_bottom_thick infill_type top_bottom_type infill_density print_speed skirt_dis
+global model_scale model_rotate_ax model_rotate_dg
+global layer_hight shell_thick top_bottom_thick solid_type infill_density print_speed skirt_dis 
+global adptive min_layer_hight adptive_shell_only support adptive_no_support curved
 %% define parameter
-% printer parameter¡ý
-bed_hight=400;bed_width=300; %bed hight and width(mm)
+% printer parameter↓
+bed_hight=300;bed_width=300; %bed hight and width(mm)
 bed_temp=60; %bed temperature(C)
-nozzle_dim=0.4; %nozzle diameter(mm)
+nozzle_dim=.4; %nozzle diameter(mm)
 filament_dim=1.75; %filament diameter(mm)
 filament_temp=200; %filament temperature(C)
-bed_center00=false; %bed center coordinate
-% slice parameter¡ý
+bed_center00=false; %bed center coordinate true
+% model process↓
+model_scale=1; %zoom in model
+model_rotate_ax='x'; %model rotate axis
+model_rotate_dg=0; %model rotate degree
+% slice parameter↓
 layer_hight=.2; %layer height(mm)
 shell_thick=1; %shell thickness(mm)
-top_bottom_thick=1; %top and bottom surface thickness(mm)
-infill_type='rec'; %rec and offset is optional
-top_bottom_type='rec'; %rec and offset is optional
-infill_density=25; %0->100 by percent
+top_bottom_thick=.3; %top and bottom surface thickness(mm)
+solid_type='rec'; %rec and offset is optional
+infill_density=12; %0->100 by percent
 print_speed=60; %nozzle move speed(mm/s)
-skirt_dis=5; %skirt distance
-% experiment function¡ý
-adptive=0; %adptive:0 equal thickness, 1 adptive
+skirt_dis=0; %skirt distance
+% experiment function↓
+adptive=0; %adptive:0 equal thickness, 1 area diff adptive,2 mesh angle adptive
+min_layer_hight=0.02; %minimal layer height for adptive
+adptive_shell_only=0; %only outer shell adaptive
+support=0; %generate support
+adptive_no_support=0; %keep this value 0!!
+curved=0; %curved slice
 % file input/output 
-filename='col.stl';
+filename='handle.stl';
 gfilename='test.gcode';
-% parameter¡ýdo not modify
-%movelist={'layer type' 'layer hight' 'layer polygon' 'layer shell' 'layer infill'};
-%I suggest not modify anything below except rotate option¡ý¡ý¡ý
+% parameter↓do not modify
+%layerlist={'layer type' 'layer hight' 'worst angle' 'layer polygon' 'solid polygon' 'normal polygon' 'support polygon'};
+%movelist={'layer type' 'layer hight' 'layer shell' 'layer solid' 'layer infill' 'layer support'};
+%I suggest not modify anything below except rotate option↓↓↓
 %% file I/O
 tic
 triangles = read_stl_file(filename);
 timeio=toc;
 fprintf('File read done, %.4f sec elapsed\n',timeio);
-%% model rotate
-%triangles=triangles*2;
-%model scale, 1 for 100%
-triangles = rotate_model(triangles,'x',90);
-triangles = [triangles(:,1:12),min(triangles(:,[3 6 9]),[],2), max(triangles(:,[ 3 6 9]),[],2)];
-timemr=toc;
-fprintf('Model rotate done, %.4f sec elapsed\n',timemr);
-%% generate layer
-if adptive==1
-    [movelist(:,1), movelist(:,2), movelist(:,3)] = adptive_polygon(triangles);
+%% model process
+[triangles,x_min,x_max,y_min,y_max]=model_process(triangles);
+%            1   2   3   4   5   6   7   8   9  10 11 12  13   14   15
+%triangles=[vx1 vy1 vz1 vx2 vy2 vz2 vx3 vy3 vz3 nx xy xz minz maxz angle]
+timemp=toc;
+fprintf('Model process done, %.4f sec elapsed\n',timemp);
+%% generate polygon
+if adptive==2
+    [layerlist(:,1),layerlist(:,2),layerlist(:,3),layerlist(:,4)] = adptive_mesh_angle(triangles);
+elseif adptive==1
+    [layerlist(:,1),layerlist(:,2),layerlist(:,3),layerlist(:,4)] = adptive_area_diff(triangles);
 else
-    [movelist(:,1), movelist(:,2), movelist(:,3)] = generate_polygon(triangles);
+    [layerlist(:,1),layerlist(:,2),layerlist(:,3),layerlist(:,4)] = generate_polygon(triangles);
 end
+timegp=toc;
+fprintf('Generate polygon done, %.4f sec elapsed\n',timegp);
+%% generate layer
+[layerlist(:,5),layerlist(:,6),layerlist(:,7)]=generate_layer(layerlist(:,[3,4,2]));
 timegl=toc;
 fprintf('Generate layer done, %.4f sec elapsed\n',timegl);
 %% generate shell
-[movelist(:,4)] = generate_shell(movelist(:,3));
-timegs=toc;
+movelist=layerlist(:,1:2);
+[movelist(:,3)] = generate_shell(layerlist(:,4));
+timegs=toc; 
 fprintf('Generate shell done, %.4f sec elapsed\n',timegs);
 %% generate infill
-x_min=min(min(triangles(:,1:3:9)));
-x_max=max(max(triangles(:,1:3:9)));
-y_min=min(min(triangles(:,2:3:9)));
-y_max=max(max(triangles(:,2:3:9)));
-row_index=find([movelist{:,1}]==1);
-[movelist(row_index,5)] = generate_infill(movelist(row_index,3), x_min, x_max, y_min, y_max);
+row_index=find([layerlist{:,1}]==1);
+[movelist(row_index,4),movelist(row_index,5),movelist(row_index,6)] = generate_infill(layerlist(row_index,[5 6 7]), x_min, x_max, y_min, y_max);
 timegi=toc;
 fprintf('Generate infill done, %.4f sec elapsed\n',timegi);
 %% export gcode
-statu=exp_gcode2(movelist(:,2), movelist(:,4), movelist(:,5), gfilename);
+gfile_statu=exp_gcode(movelist(:,2), movelist(:,3), movelist(:,4), movelist(:,5), movelist(:,6),gfilename);
 timeeg=toc;
-if statu==1
+if gfile_statu==1
     fprintf('Gcode file has written to %s\nElapsed time %.4f sec\n',gfilename,timeeg);
 end
 %% plot
 hold on;
 view(15,23);
-axis equal;
-plot_slices(movelist(:,2),movelist(:,4), 0)
-%plot shell
-plot_slices(movelist(:,2),movelist(:,5), 0)
-%plot infill
-
-        
-
-
-
+axis equal off;
+plot_slices(movelist(:,2),movelist(:,3), 0)
+%% print time estimate
+printtime= time_estimate(gfilename);
+fprintf('Print time estimate %.0f hours %.0f minute\n',fix(printtime),rem(printtime,1)*60);
